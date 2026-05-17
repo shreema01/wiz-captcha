bda44ade3ad3850b0de99e80839d4a82b946905d
Shreema Dey Purba <purbadey46@gmail.com>
2026-05-17 10:46:58 +0600

    modification

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -9,9 +9,6 @@
     <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-
     <style>
         * {
             margin: 0;
