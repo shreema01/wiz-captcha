40a7a8988f093c32c0430cafec513e11dc3f3d52
Shreema Dey Purba <purbadey46@gmail.com>
2026-06-02 12:19:58 +0600

    modification

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::post('/contact', function (Request $request) {
 |--------------------------------------------------------------------------
 */
 
+
 Route::get('/login', function () {
     return view('login');
 })->name('login');
