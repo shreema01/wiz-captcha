cadced0ec90aacecb7ee1307825e8715fd63cbd9
Shreema Dey Purba <purbadey46@gmail.com>
2026-06-15 10:56:17 +0600

    modification

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,7 +6,6 @@
     <button type="button" onclick="refreshCaptcha()">Refresh</button>
 
     <input type="text" name="captcha" required autocomplete="off">
-
     
     @error('captcha')
         <div>{{ $message }}</div>
