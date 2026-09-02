08206375002e0e7d6ff4deea3520e797999ba747
Shreema Dey Purba <purbadey46@gmail.com>
2026-09-02 16:43:56 +0600

    modification

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -40,6 +40,7 @@
             from { opacity: 0; transform: translateY(20px); }
             to { opacity: 1; transform: translateY(0); }
         }
+        
         h2 {
             text-align: center;
             margin-bottom: 25px;
